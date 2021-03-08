b72f72ae63d02cb5658ad41b7a642fb8ee9345ad
Lenar <flfforwork@gmail.com>
2021-03-08 09:13:49 +0300

    feat: implement max, min and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (!array) return 0
+  let minNumber = array[0];
+  if (minNumber === undefined) return 0
+  for(let i = 1; i < array.length; i++) {
+  if (array[i] < minNumber) minNumber = array[i];
+    }
+    return minNumber
 }
 
+
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0
+  let maxNumber = array[0];
+  if (maxNumber === undefined) return 0  
+  for(let i = 1; i < array.length; i++) {
+  if (array[i] > maxNumber) maxNumber = array[i];
+    }
+    return maxNumber
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0
+  let sum = 0;
+  for(let i = 0; i < array.length; i++) {
+  sum += array[i]
+    }
+    if (isNaN(sum / array.length)) return 0
+    return sum / array.length
 }
